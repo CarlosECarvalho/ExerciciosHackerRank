4394aa6b64e29ac406945a3ddd5d51c39d42de23
Carlos Eduardo <caducferreira2@gmail.com>
2024-09-03 23:27:55 -0300

    Correcao do nome do arquivo e acrescentado novos exercicios e readme

---
+++ ExercicioSQL.sql
@@ -0,0 +1,29 @@
+Exercicio:
+https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+Solução
+Select * from CITY where CountryCode = 'USA' and Population >= 100000
+
+Exercicio:
+https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+Solução
+select Name from City where CountryCode = 'USA' and Population >= 120000
+
+Exercicio:
+https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+Solução
+select * from CITY
+
+Exercicio:
+https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+Solução
+select * from CITY where ID = 1661
+
+Exercicio:
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+Solução
+select * from CITY where COUNTRYCODE = 'JPN'
+
+Exercicio:
+https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+Solução
+select NAME from CITY where COUNTRYCODE = 'JPN'
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ExerciciosHackerRank
 Um repositório no qual vou salvar os exercícios que vou fazendo no site hackerrank.com
+
+### Exercicios de SQL
+A primeira lista de exercícios será em SQL, para aplicar o conhecimento que aprendi no último curso que conclui.
+[Lista de ExerciciosSQL resolvida]()
